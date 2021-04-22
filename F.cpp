8290308536f75592c6facc6d1a88f1c438c2aa5c
Sheetal Patil <sheetal.patil@ibm.com>
2021-04-22 20:08:13 +0530

    created F.cpp

---
+++ F.cpp
@@ -0,0 +1,28 @@
+<Html>  
+<head>   
+<title>  
+Registration Page  
+</title>  
+</head>  
+<br>  
+<br>  
+<form>  
+  
+<label> Firstname </label>         
+<input type="text" name="firstname" /> <br> <br>  
+<label> Middlename: </label>     
+<input type="text" name="middlename" /> <br> <br>  
+<label> Lastname: </label>         
+<input type="text" name="lastname" /> <br> <br>  
+  
+<br>  
+Email:  
+<input type="email"  name="email"/> <br>    
+<br> <br>  
+Password:  
+<input type="Password"  name="pass"> <br>   
+<br> <br>  
+<input type="submit" value="register"/>  
+</form>  
+</body>  
+</html>  
