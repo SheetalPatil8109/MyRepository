896d52e0fa9ebb8d3b74d898886c44270bc50e0b
SheetalPatil8109 <82994825+SheetalPatil8109@users.noreply.github.com>
2021-04-22 19:41:39 +0530

    Create D.cpp

---
+++ D.cpp
@@ -0,0 +1,15 @@
+<html>
+  <body>
+    <head>please login</head>
+    <form action="welcome.html">
+      <h3>
+      username:<input type="text" name="uname"><br>
+      password:<input type="password" value="password">
+      <br>
+      <input type="submit" value="LOGIN">
+      <input type="submit"value="RESET">
+      </h3>
+    </form>
+  </body>
+</html>
+  
