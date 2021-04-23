6cc49815afd1fdd95f36aab5dce95ef3ded77987
SheetalPatil8109 <82994825+SheetalPatil8109@users.noreply.github.com>
2021-04-23 08:01:53 +0530

    Update F.cpp

--- F.cpp
+++ F.cpp
@@ -1,28 +1,7 @@
-<Html>  
-<head>   
-<title>  
-Registration Page  
-</title>  
-</head>  
-<br>  
-<br>  
-<form>  
-  
-<label> Firstname </label>         
-<input type="text" name="firstname" /> <br> <br>  
-<label> Middlename: </label>     
-<input type="text" name="middlename" /> <br> <br>  
-<label> Lastname: </label>         
-<input type="text" name="lastname" /> <br> <br>  
-  
-<br>  
-Email:  
-<input type="email"  name="email"/> <br>    
-<br> <br>  
-Password:  
-<input type="Password"  name="pass"> <br>   
-<br> <br>  
-<input type="submit" value="register"/>  
-</form>  
-</body>  
-</html>  
+#include<iostream>
+int main()
+{
+
+std::cout<<"welcome to git";
+return 0;
+
